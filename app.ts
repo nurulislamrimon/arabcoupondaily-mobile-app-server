21ff19c8d7d136bb5acba23d993f4b04ee1f8806
nurulislamrimon <nurulislamrimon@gmail.com>
2023-09-18 22:48:55 +0600

    cors resolved

--- app.ts
+++ app.ts
@@ -11,12 +11,7 @@ const app = express();
 // configuration
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(urlencoded({ extended: true }));
 // home route
 app.get("/", (req: Request, res: Response) => {
